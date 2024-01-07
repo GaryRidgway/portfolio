6f065d9e1fc06444115e806a76ee3b2132ee14cd
Gary Ridgway <gary-ridgway@uiowa.edu>
2024-01-06 23:23:41 -0600

    colors and stuv.

--- src/components/Display.vue
+++ src/components/Display.vue
@@ -1,36 +1,40 @@
 <script setup>
 defineProps({
-  w: String,
-  h: String
+    title: String,
+    w: String,
+    h: String
 })
 </script>
 
 <template>
     <div class="display">
+        <h1 v-if="title" class="title" >{{ title }}</h1>
         <div class="case">
-    <component v-if="w && h" :is="'style'">
-        .case {
-            height: {{ h }};
-        }
+            <component v-if="w && h" :is="'style'">
+                .case {
+                height: {{ h }};
+                }
 
-        iframe {
-            width: {{ w }};
-            height: {{ h }};
-        }
-    </component>
-    <slot name="case"></slot> 
-  </div>
-  <slot name="statement"></slot> 
+                iframe {
+                width: {{ w }};
+                height: {{ h }};
+                }
+            </component>
+            <slot name="case"></slot>
+        </div>
+        <slot name="statement"></slot>
     </div>
-  
 </template>
 
 <style lang="scss" scoped>
-
 .display {
     max-width: var(--content-max-width);
     padding: var(--spacing);
     margin: 0 auto;
+
+    .title {
+        font-family: 'Cinzel Decorative', serif;
+    }
 
     .case {
         float: right;
