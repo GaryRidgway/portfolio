d05fc0e87de28c2a3316da54ab7183ac4f42609a
Alan Ridgway <gary-ridgway@uiowa.edu>
2024-01-17 00:51:37 -0600

    Are gradients still cool?

--- src/components/Background.vue
+++ src/components/Background.vue
@@ -4,6 +4,7 @@
 <template>
   <div class="bg-image">
     <img :src="bgUrl">
+    <div class="gradient"></div>
   </div>
 </template>
 
@@ -11,12 +12,12 @@
 .bg-image {
   position: absolute;
   z-index: -1;
-  opacity: 0.08;
   width: 100vw;
   height: 100vh;
   overflow: hidden;
 
   img {
+    opacity: 0.2;
     width: 200%;
     height: 200%;
     object-fit: cover;
@@ -25,6 +26,14 @@
     top: -50%;
     transform: rotate(-30deg);
     filter: saturate(3) contrast(1.2);
+  }
+
+  .gradient {
+    position: absolute;
+    width: 100%;
+    height: 100%;
+    background: rgb(25,25,25);
+    background: linear-gradient(155deg, rgba(25,25,25,0) 0%, rgba(25,25,25,0.34499737394957986) 21%, rgba(25,25,25,0.6783307072829132) 38%, rgba(25,25,25,0.8996192226890756) 64%, rgba(25,25,25,0.8968181022408963) 100%);
   }
 }
 </style>
